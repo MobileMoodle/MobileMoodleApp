e9458d51f6e52306334880bd7f46127d50f57103
Schillinger Alex <schil399@umn.edu>
2014-10-07 20:57:15 -0500

    Corrected Action Bar Iconography

--- MoodleManaged/gen/edu/umn/moodlemanaged/R.java
+++ MoodleManaged/gen/edu/umn/moodlemanaged/R.java
@@ -1068,8 +1068,9 @@ containing a value of this type.
         public static final int abc_textfield_searchview_holo_light=0x7f020054;
         public static final int abc_textfield_searchview_right_holo_dark=0x7f020055;
         public static final int abc_textfield_searchview_right_holo_light=0x7f020056;
-        public static final int ic_action_search=0x7f020057;
-        public static final int ic_launcher=0x7f020058;
+        public static final int ic_action_overflow=0x7f020057;
+        public static final int ic_action_search=0x7f020058;
+        public static final int ic_launcher=0x7f020059;
     }
     public static final class id {
         public static final int action_bar=0x7f05001c;
@@ -1232,8 +1233,8 @@ containing a value of this type.
         /**  Description of a share target (both in the list of such or the default share button) in a ShareActionProvider (share UI). [CHAR LIMIT=NONE] 
          */
         public static final int abc_shareactionprovider_share_with_application=0x7f0a000b;
+        public static final int action_overflow=0x7f0a0011;
         public static final int action_search=0x7f0a0010;
-        public static final int action_settings=0x7f0a0011;
         public static final int app_name=0x7f0a000d;
         public static final int hello_world=0x7f0a000f;
         public static final int title_activity_main=0x7f0a000e;
