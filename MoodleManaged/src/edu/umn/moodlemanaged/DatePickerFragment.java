103eb5e5f6b69ae309ce8dc26075a10eb48b0500
Kent Sommer <kent.sommer13@gmail.com>
2014-11-30 21:48:18 -0600

    Added ability to add events

--- MoodleManaged/src/edu/umn/moodlemanaged/DatePickerFragment.java
+++ MoodleManaged/src/edu/umn/moodlemanaged/DatePickerFragment.java
@@ -4,10 +4,15 @@ import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.widget.DatePicker;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.text.Format;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 
 public class DatePickerFragment extends DialogFragment implements
         DatePickerDialog.OnDateSetListener
@@ -25,30 +30,18 @@ public class DatePickerFragment extends DialogFragment implements
         return new DatePickerDialog(getActivity(), this, year, month, day);
     }
 
-    public void onDateSet(DatePicker view, int year, int month, int day) {
-//        TextView date = (TextView)getActivity().findViewById(R.id.dateText);
-//        if(day < 10)
-//        {
-//            if(month < 10)
-//            {
-//                date.setText("Date: 0" + month + "/0" + day + "/" + year);
-//            }
-//            else
-//            {
-//                date.setText("Date: " + month + "/0" + day + "/" + year);
-//            }
-//        }
-//        else
-//        {
-//            if(month < 10)
-//            {
-//                date.setText("Date: 0" + month + "/" + day + "/" + year);
-//            }
-//            else
-//            {
-//                date.setText("Date: " + month + "/" + day + "/" + year);
-//            }
-//        }
+    public void onDateSet(DatePicker view, int year, int month, int day)
+    {
+        String s;
+        Format formatter;
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.YEAR, year);
+        calendar.set(Calendar.MONTH, month);
+        calendar.set(Calendar.DAY_OF_MONTH, day);
+
+        formatter = new SimpleDateFormat("yyyy/MM/dd ");
+        s = formatter.format(calendar.getTime());
+        NewAssignmentActivity.tempEventDate = s;
     }
 }
 
